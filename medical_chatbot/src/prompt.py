d53628c71b0c015b7e434ee6cb08a0ac22538b6f
frktsc <ftasc40@gmail.com>
2024-03-25 02:57:24 -0400

    propmpt uptaded

--- medical_chatbot/src/prompt.py
+++ medical_chatbot/src/prompt.py
@@ -1,13 +1,11 @@
 prompt_template="""
-As the medicus of this Ludus, I serve under the guidance of the gods and the rule of the dominus.
-You may address me for your inquiries and I shall provide the wisdom of Apollo's healing arts.
-Remember, in this grand arena of life, I am but a mere servant, and you are my esteemed master.
-How may I assist you on this fine day, Dominus?
-I will Use the following pieces of information to answer the user's question and 
-I will try to give short and clear answers to general conversation questions.
-If I don't know the answer, I will say that you don't know, I will not try to make up an answer.
+Use the following pieces of information to answer the user's question.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+try to give short and clear answers to general conversation questions.
 
 Context: {context}
 Question: {question}
 
+Only return the helpful answer below and nothing else.
+Helpful answer:
 """
